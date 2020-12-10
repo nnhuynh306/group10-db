3cbb135cdfd7a55d0493c0f9aaca42b19208cc08
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2020-12-10 13:24:55 +0700

    Chỉnh Search

--- routes/Search.js
+++ routes/Search.js
@@ -1,22 +1,17 @@
 const express = require('express')
 const router = express.Router();
-const controller = {}
 const Op = require('Sequelize').Op;
 
 const models = require('../models')
-
-
-
 
 
 router.get('/', function(req, res) {
     res.render('search')
 })
 
-router.post('/keyword', (req, res) => {
-    let keyword = 'e';
-    // console.log('Got body:', req.body.keyword);
-    // res.render('search');
+
+router.get('/keyword', (req, res) => {
+    let keyword = req.query.keyword;
     let temp = '%' +  keyword + '%';
     models.Recipe.findAll({
         where: {
@@ -38,11 +33,10 @@ router.post('/keyword', (req, res) => {
                 },
             ]
         },
-        include: [models.Direction, models.Ingredient]
     })
     .then(data => {
         res.json(data)
     })
-  });
+});
 
-  module.exports = router
+module.exports = router
