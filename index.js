8ed7555d00daeaf52501b65d69b9d4f87aaf7cd1
Nam Huynh Nhat <nnhuynh306@gmail.com>
2020-12-10 10:06:32 +0700

    add hbs folders

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'))
+
+const ehbs = require('express-handlebars')
+
+app.engine('hbs', ehbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layout',
+    partialsDir: __dirname + '/views/partials',
+}))
+
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send('HOMEPAGE')
+    res.render('homepage')
 })
+
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), (req, res) => {
