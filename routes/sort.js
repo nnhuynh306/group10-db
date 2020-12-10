3467050ded0e34ca854330874508be4a3df52aae
thinhnguyenphuc <thinhnguyenphuc55@gmail.com>
2020-12-10 12:22:07 +0700

    Finish sort

--- routes/sort.js
+++ routes/sort.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res) {
   let sortController = require('./../controller/sortController');
     sortController.getAll().then(data=> {
       res.locals.data=data;
-      res.render('./../views/sort',()=>{
-        nav.sortClass=current
+      res.render('./../views/sort',{
+        sortClass:'current'
       });
     });  
   })
@@ -15,17 +15,55 @@ router.get('/', function(req, res) {
 router.get('/AtoZ', function(req, res) {
   let sortController = require('./../controller/sortController');
   sortController.getAll().then(data=> {
+    function compare(a, b) {
+      const A = a.title.toUpperCase();
+      const B = b.title.toUpperCase();
+    
+      let comparison = 0;
+      if (A >B) {
+        comparison = 1;
+      } else if (A <B) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+    data.sort(compare);
     res.locals.data=data;
-    res.render('./../views/sort');
+    res.render('./../views/sort',{
+      sortClass:'current'
+    });
   }); 
   })
 router.get('/ZtoA', function(req, res) {
   let sortController = require('./../controller/sortController');
   sortController.getAll().then(data=> {
+    function compare(a, b) {
+      const A = a.title.toUpperCase();
+      const B = b.title.toUpperCase();
+    
+      let comparison = 0;
+      if (A >B) {
+        comparison = -1;
+      } else if (A <B) {
+        comparison = 1;
+      }
+      return comparison;
+    }
+    data.sort(compare);
     res.locals.data=data;
-    res.render('./../views/sort');
+    res.render('./../views/sort',{
+      sortClass:'current'
+    });
   }); 
   })
-
+  router.get('/sort', function(req, res) {
+    let sortController = require('./../controller/sortController');
+      sortController.getAll().then(data=> {
+        res.locals.data=data;
+        res.render('./../views/sort',{
+          sortClass:'current'
+        });
+      });  
+    })
 
 module.exports = router
